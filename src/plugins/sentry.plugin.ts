a399da812febcf54461fd0be1f8d26f65d87008f
LuismarPavani <luismar_pavani@hotmail.com>
2020-06-06 14:07:13 -0300

    serve

--- src/plugins/sentry.plugin.ts
+++ src/plugins/sentry.plugin.ts
@@ -1,16 +1,20 @@
-import { configureScope, init } from "@sentry/browser"
+import { configureScope, init } from '@sentry/browser';
 
 (() => {
-  if (window.location.hostname == 'localhost' || window.location.hostname == '127.0.0.1'){
-      return;
-  }
 
-  const { REACT_APP_SENTRY_DSN } = process.env;
-  
-  init({dsn: REACT_APP_SENTRY_DSN });
-  
-  configureScope(scope => {
-      
-  })
-}
-)
+    if (window.location.hostname === 'localhost' || 
+        window.location.hostname === '127.0.0.1') {
+        return;
+    }
+
+    const { REACT_APP_SENTRY_DSN } = process.env;
+    if (!REACT_APP_SENTRY_DSN) {
+        return;
+    }
+
+    init({ dsn: REACT_APP_SENTRY_DSN});
+
+    configureScope(scope => {
+
+    });
+})();
