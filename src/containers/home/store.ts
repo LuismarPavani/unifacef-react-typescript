7744ddca7919dbb10b0557e744f7bfd66404e8b0
LuismarPavani <luismar_pavani@hotmail.com>
2020-06-06 11:10:32 -0300

    commit

--- src/containers/home/store.ts
+++ src/containers/home/store.ts
@@ -5,12 +5,13 @@ export default class HomeStore {
   @observable records: any[] = [];
 
   @action buildRecords = async () => {
-      try{
-    const { data } = await getPrice();
-    this.records = Object.values(data);
-} catch (error){
-    
-}
+   try {
+      const { data } = await getPrice();
+      this.records = Object.values(data);
+    } catch (error) {
+      this.records = [];
+      throw new Error('Falha ao obter cotação');
+    }
   }
 
 }
