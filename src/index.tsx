c5388718fbcc6e30adc5ee57ec0ed3175caf8520
LuismarPavani <luismar_pavani@hotmail.com>
2020-06-06 13:53:00 -0300

    pluguin

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
+import './index.css';
+import './plugins/sentry.plugin';
+import 'semantic-ui-css/semantic.min.css';
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import * as store from './mobx';
+
+import { Provider } from 'mobx-react';
+import { Router } from 'react-router-dom';
 import Routes from './routes';
 import { createBrowserHistory } from 'history';
-import { Provider } from 'mobx-react';
 import { router } from './mobx/';
-import { Router } from 'react-router-dom';
 import { syncHistoryWithStore } from 'mobx-react-router';
-import './index.css';
-import 'semantic-ui-css/semantic.min.css';
 
 const rootElement = document.getElementById('root');
 const browserHistory = createBrowserHistory();
