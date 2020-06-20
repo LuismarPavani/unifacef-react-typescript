63b49141caca20094b249544c470d20a25a90894
LuismarPavani <luismar_pavani@hotmail.com>
2020-06-20 08:33:57 -0300

    Aula 2

---
+++ src/apis/star-wars.api.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+// https://star-wars-api-luismar.herokuapp.com
+// https://star-wars-api-unifacef.herokuapp.com
+
+const baseURL = process.env.REACT_APP_STAR_WARS_BASE_URL;
+
+export const getFilms = async () => {
+    return axios.request({ baseURL, url: 'films' })
+  }
+  
+  export const getFilmById = async (id: number) => {
+    return axios.request({ baseURL, url: `films/${id}` })
+  }
+  
