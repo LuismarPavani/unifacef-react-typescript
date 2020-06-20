e010309a9e6b80a569842922681ca12a192e3a45
LuismarPavani <luismar_pavani@hotmail.com>
2020-06-20 09:31:26 -0300

    aula 2

--- src/apis/star-wars.api.ts
+++ src/apis/star-wars.api.ts
@@ -6,7 +6,7 @@ import axios from 'axios';
 const baseURL = process.env.REACT_APP_STAR_WARS_BASE_URL;
 
 export const getFilms = async () => {
-    return axios.request({ baseURL, url: 'films' })
+    return axios.request({ baseURL, url: `films` })
   }
   
   export const getFilmById = async (id: number) => {
--- src/routes/endpoints.ts
+++ src/routes/endpoints.ts
@@ -2,6 +2,7 @@ import Combustivel from '../containers/combustivel';
 import Home from '../containers/home';
 import { RouteProps } from 'react-router-dom';
 import Sobre from '../containers/sobre';
+import StarWars from '../containers/star-wars';
 
 const publicUrl = process.env.PUBLIC_URL;
 
@@ -13,5 +14,6 @@ export const endpoints: EndPointsProps[] = [
   { path: `${publicUrl}/`, component: Home, exact: true },
   { path: `${publicUrl}/home`, name: 'Home', component: Home, exact: true },
   { path: `${publicUrl}/combustivel`, name: 'Combustível', component: Combustivel, exact: true },
+  { path: `${publicUrl}/star-wars`, name: 'Star Wars', component: StarWars, exact: true },
   { path: `${publicUrl}/sobre`, name: 'Sobre', component: Sobre, exact: true },
 ];
