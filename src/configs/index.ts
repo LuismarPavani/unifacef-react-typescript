0273acbf7a6eeb43a69cd245ece9941e69d2a58d
LuismarPavani <luismar_pavani@hotmail.com>
2020-07-04 09:26:17 -0300

    one-signal

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,10 +1,7 @@
 export const configs = {
     apis: {
-      correios: process.env.REACT_APP_CEP_URL,
       economia: process.env.REACT_APP_ECONOMIA_URL,
-      github: process.env.REACT_APP_GITHUB_URL,
-      starWars: process.env.REACT_APP_STAR_WARS_BASE_URL,
-      corona: process.env.REACT_APP_CORONA_URL,
+      starWars: process.env.REACT_APP_STAR_WARS_URL,
     },
     sentry: process.env.REACT_APP_SENTRY_DNS,
     onesignal: process.env.REACT_APP_ONE_SIGNAL!
--- src/plugins/one-signal.plugin.ts
+++ src/plugins/one-signal.plugin.ts
@@ -1,10 +1,7 @@
 import OneSignal from 'react-onesignal';
 import { configs } from '../configs';
 
-const options = { 
-    autoRegister: true, 
-    autoResubscribe: true, 
-    notifyButton: { enable: true } }
+const options = { autoRegister: true, autoResubscribe: true, notifyButton: { enable: true } }
 
 OneSignal.initialize(configs.onesignal, options);
 
